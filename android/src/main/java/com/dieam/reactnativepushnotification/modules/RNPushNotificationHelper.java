d6936f291ee3c5c5e62617118ecfddd1a29e0d46
Israel Roth <israel@kanghealth.io>
2019-10-15 15:59:41 +0300

    add back import of List

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -30,6 +30,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.util.Arrays;
+import java.util.List;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
