c687a9c3cf465b58dfaa389ce77edfcb51499e84
Israel Roth <israel@kanghealth.io>
2019-10-15 22:30:31 +0300

    remove another log file to make Veracode happy

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -140,7 +140,8 @@ public class RNPushNotificationHelper {
 
         boolean isSaved = scheduledNotificationsPersistence.contains(id);
         if (!isSaved) {
-            Log.e(LOG_TAG, "Failed to save " + id);
+            // Log.e(LOG_TAG, "Failed to save " + id);
+            Log.e(LOG_TAG, "Failed to save ");
         }
 
         sendNotificationScheduledCore(bundle);
